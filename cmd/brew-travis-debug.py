c5f1e390df75b90992505ec3df4fa369e7fd06c5
Bob W. Hogg <rwhogg@linux.com>
2016-08-28 12:39:55 -0400

    Initial commit

---
+++ cmd/brew-travis-debug.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+#
+# Description: Test a Homebrew formula in the Travis CI environment 
+# Author: Bob W. Hogg
+# Usage: FIXME FIXME FIXME
+#
+
+from distutils.spawn import find_executable;
+import subprocess;
+import sys;
+
+docker = find_executable("docker");
+if not docker:
+    print "Error: please run \"brew install docker\""
+    sys.exit(1);
+
+my_image_tag = "rwhogg/homebrew-travis-debug";
+subprocess.call([docker, "build", ".", "-t", my_image_tag]);
+
+# FIXME: the command is WRONG
+subprocess.call([docker, "run", "-it", "/bin/bash");
